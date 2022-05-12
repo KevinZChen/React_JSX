3791fbd4af6b3362c22759fac16c097794275099
Kevin Chen <97408222+KevinZChen@users.noreply.github.com>
2022-05-12 09:26:08 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//Create a react app from scratch.import React from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <ul>
+      <li>its lit</li>
+      <li>its lit</li>
+      <li>its lit</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
